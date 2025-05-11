5a41522244185c73e1339bd5ab010d96dcd319e0
CHEVURI JAHNAVI <2300030131@kluniversity.in>
2025-05-11 08:31:39 +0530

    Update AdminHome.jsx

--- src/admin/AdminHome.jsx
+++ src/admin/AdminHome.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import config from '../config';
-import './Admin.css'; // Make sure this file exists
+import './admin.css'; // Make sure this file exists
 
 export default function AdminHome() {
   const [customerCount, setCustomerCount] = useState(0);
